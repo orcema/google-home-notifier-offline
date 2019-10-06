08f238ccb8262ce0eddc6131e5a65a54ad912a3b
Orcé Marinkovski <orce00@gmail.com>
2019-10-06 20:39:02 +0200

    fix: when casting to multiple devices restoring devices didn't work thus all the devices remained at the announcement volume level.

--- google-home-notifier-offline.js
+++ google-home-notifier-offline.js
@@ -2,7 +2,7 @@
 
 var Client = require('castv2-client').Client;
 const EventEmitter = require('events');
-var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+
 var Googletts = require('google-tts-api');
 var net = require('net');
 var fs = require('fs');
@@ -14,11 +14,14 @@ var path = require('path');
 
 var actualVolume=0; // actual Volume of the assistant
 var emitVolume=20/100; // volume level to use for notificaiton/play
-var playerState="IDLE"; // holds the player status
+var previousPlayerState="IDLE"; // holds the player status
 var cacheFolder = "" // default init to no cache folder
 var httpServer = "";
 var httpServerPort ="8081"; // default port for serving mp3
 var serverIP="";
+
+var timeoutRestoreDevicesVolume = null;
+
 
 function localFileServerClose(callback){
   if (httpServer!=="") {
@@ -171,37 +174,94 @@ function GoogleHomeNotifier(deviceip, language, speed) {
     });
   };
 
+  var restorVolumeOfDevice = function (host,volume){
+    var client = new Client();
+    client.connect(host, function () {
+      client.setVolume({ level: volume },function(err,response){
+        console.log("volume restored to ", volume," for device", host);
+      }); 
+    });
+  }
+
+  var restorVolumeOfCastedDevies = function(){
+    timeoutRestoreDevicesVolume = null;
+    for (var host in global.castedDevices) {
+      new restorVolumeOfDevice(host,global.castedDevices[host].actualVolume);
+    }  
+  }
+
   var onDeviceUp = function (host, url, callback) {
+    if (global.castedDevices === undefined){
+      global.castedDevices ={};
+    }
+    var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
+
+    console.log ("new message -----");
     var client = new Client();
     var clienttcp = new net.Socket();
     clienttcp.connect(8009, host, function () {
+      global.castedDevices[host]={};
       client.connect(host, function () {
         client.getVolume(function(err,volume){
-          actualVolume=volume.level;
+          global.castedDevices[host].actualVolume = volume.level; 
+          console.log("inital vol level",global.castedDevices[host].actualVolume,"host",host);
           client.setVolume({ level: emitVolume },function(err,response){ // set the notification volume
+            if (timeoutRestoreDevicesVolume == null){
+              timeoutRestoreDevicesVolume = setTimeout(function(){
+                console.warn("fallback timeout triggered for restoring devices initial volume on host ", host);
+                restorVolumeOfCastedDevies();
+              },10000);
+            }
+            console.log("Vol level set to ",emitVolume,"host",host);
             client.launch(DefaultMediaReceiver, function (err, player) {
+              if (err){
+                console.error(err);
+              }
               var media = {
                 contentId: url,
                 contentType: 'audio/mp3',
                 streamType: 'BUFFERED' // or LIVE
               };
               player.on('status', function(status) {
-                // console.log('status broadcast playerState=%s', status.playerState);
-                if ((playerState==="PLAYING"||playerState==="BUFFERING") && status.playerState==="IDLE"){
-                  // reset volume to initial level and close the connection
-                    client.setVolume({ level: actualVolume },function(err,response){
-                      setTimeout(function(){
-                        client.close();
-                        callback('Device notified');
-                      },1000);
-                    });
+                var currentPlayerState = status.playerState;
+                console.log('status broadcast currentPlayerState=%s', currentPlayerState,"for host",host);
+
+                if (currentPlayerState === "PLAYING"){
+                  if (timeoutRestoreDevicesVolume != null){
+                    clearTimeout(timeoutRestoreDevicesVolume);
+                    timeoutRestoreDevicesVolume = null;
+                  }
                 }
-                playerState=status.playerState; // save current player state
+
+                if (currentPlayerState === "PAUSED"){
+                  restorVolumeOfCastedDevies();
+                }
+
+
+                var finishedPlaying = (previousPlayerState === "PLAYING"|| previousPlayerState === "BUFFERING") && currentPlayerState === "IDLE";
+                if (finishedPlaying){
+                  // reset volume to initial level and close the connection
+                    // client.setVolume({ level: global.castedDevices[host].actualVolume },function(err,response){
+                    //   console.log("Vol level restored to ",global.castedDevices[host].actualVolume,"host",host);
+                    setTimeout(function(){
+                        restorVolumeOfCastedDevies();
+                        // client.close();
+                        // console.log("Connection closed to host ",host);
+                        // callback('Device notified');
+                      },1000);
+                    // });
+                }else{
+                  // console.log("still playing for host ", host);
+                }
+                
+                previousPlayerState=currentPlayerState; // save current player state
+                // console.log("previousPlayerState set to ",previousPlayerState , "for host ", host);
+
               });              
               player.load(media, {
                 autoplay: true
               }, function (err, status) {
-                // console.log(status); 
+                // console.log("loading:",status); 
               });
             });
           });
