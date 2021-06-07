74b01c7b40a538754363bfabb62d7d69038c9972
Orcé MARINKOVSKI <Orc MARINKOVSKI>
2021-06-07 20:25:13 +0200

    mediaserver moved to node-red-contrib

--- google-home-notifier-offline.js
+++ google-home-notifier-offline.js
@@ -1,5 +1,9 @@
 'use strict';//
 
+
+
+
+
 var Client = require('castv2-client').Client;
 const EventEmitter = require('events');
 
@@ -17,71 +21,13 @@ var emitVolume = 20 / 100; // volume level to use for notificaiton/play
 var textSpeed = 1; // speak speed value between 0.1 up to 1
 var previousPlayerState = "IDLE"; // holds the player status
 var cacheFolder = "" // default init to no cache folder
-var httpServer = "";
-var httpServerPort = "8081"; // default port for serving mp3
-var serverIP = "";
+
+
 
 var timeoutRestoreDevicesVolume = null;
 
+function GoogleHomeNotifier(deviceip, language, speed,mediaServerIp, mediaServerPort,cacheFolder) {
 
-function localFileServerClose(callback) {
-  if (httpServer !== "") {
-    httpServer.close(function () {
-      httpServer = "";
-    });
-  }
-  callback();
-}
-
-function localFileServerStart() {
-  const FileServer = require('file-server');
-
-  const fileServer = new FileServer((error, request, response) => {
-    response.statusCode = error.code || 500;
-    response.end("404: Not Found " + request.url);
-  });
-
-  const serveRobots = fileServer.serveDirectory(cacheFolder, {
-    '.mp3': 'audio/mpeg'
-  });
-
-  httpServer = require('http')
-    .createServer(serveRobots)
-    .listen(httpServerPort);
-  console.log("fileServer listening on ip " + serverIP + " and port " + httpServerPort);
-
-}
-
-function localFileServerRestart() {
-  if (httpServer === "") {
-    localFileServerStart();
-  } else {
-    localFileServerClose(function () {
-      localFileServerStart();
-    })
-  }
-}
-
-function Download_Mp3(text, language, fileNameWithSpeedAndLanguage, playSlow, callback) {
-
-  var dstFilePath = path.join(cacheFolder, fileNameWithSpeedAndLanguage);
-  // get base64 text
-  Googletts
-    .getAudioBase64(text, { lang: language, slow: playSlow })
-    .then((base64) => {
-      // console.log({ base64 });
-
-      // save the audio file
-      const buffer = Buffer.from(base64, 'base64');
-      fs.writeFileSync(dstFilePath, buffer, { encoding: 'base64' });
-      callback();
-    })
-    .catch(console.error);
-}
-
-function GoogleHomeNotifier(deviceip, language, speed) {
-  var ip = require("ip");
-  serverIP = ip.address();
 
 
   this.deviceip = deviceip;
@@ -89,40 +35,6 @@ function GoogleHomeNotifier(deviceip, language, speed) {
   this.speed = speed;
 
   var emitter = this;
-  this.setSpeechSpeed = (readSpeed) => {
-    textSpeed = parseFloat(readSpeed);
-    return this;
-  }
-
-  this.setEmitVolume = function (pctVolume) {
-
-    if (pctVolume != undefined) { // if not defined then the (default/last set) emitValue will be used
-      emitVolume = pctVolume;
-      // make sure volume is a percentage, else adjust
-      if (emitVolume > 100) emitVolume = 100;
-      if (emitVolume < 0) emitVolume = 0;
-      if (emitVolume > 1) emitVolume = emitVolume / 100;
-    }
-    return this;
-  }
-
-  this.setCacheFolder = function (cacheFolderToUse) {
-    if (cacheFolderToUse !== "" && cacheFolder !== cacheFolderToUse) {
-      cacheFolder = cacheFolderToUse;
-      localFileServerRestart()
-    }
-    return this;
-  }
-
-  this.setFileServerPort = function (serverPortToSet) {
-    if (serverPortToSet !== "" && httpServerPort !== serverPortToSet) {
-      httpServerPort = serverPortToSet;
-      if (cacheFolder !== "") {
-        localFileServerRestart();
-      }
-    }
-    return this;
-  }
 
   this.notify = function (message, callback) {
     getSpeechUrl(message, this.deviceip, function (res) {
@@ -130,15 +42,7 @@ function GoogleHomeNotifier(deviceip, language, speed) {
     });
   };
 
-  this.play = function (mp3_url, callback) {
-    getPlayUrl(mp3_url, this.deviceip, function (res) {
-      emitter.emit("play", res)
-    });
-  };
-
   var getSpeechUrl = function (text, host, callback) {
-
-
     if (cacheFolder == "") {
       emitter.emit("error", "please setup a cache folder");
       return
@@ -146,7 +50,7 @@ function GoogleHomeNotifier(deviceip, language, speed) {
     let fileName = text.replace(/[^a-zA-Z0-9]/g, "_").toUpperCase();
     const fileNameWithSpeedAndLanguage = fileName + "-" + (textSpeed != 1 ? "slow" : "normal") + ".mp3";
     let fileToCheckInCache = path.join(cacheFolder, fileNameWithSpeedAndLanguage);
-    let url = "http://" + serverIP + ":" + httpServerPort + "/" + fileNameWithSpeedAndLanguage;
+    let url = "http://" + mediaServerIp + ":" + mediaServerPort + "/" + fileNameWithSpeedAndLanguage;
 
     if (fs.existsSync(fileToCheckInCache)) {
       onDeviceUp(host, url, function (res) {
@@ -161,29 +65,8 @@ function GoogleHomeNotifier(deviceip, language, speed) {
         });
       });
     }
+
   };
-
-  var getPlayUrl = function (url, host, callback) {
-    onDeviceUp(host, url, function (res) {
-      callback(res);
-    });
-  };
-
-  var restorVolumeOfDevice = function (host, volume) {
-    var client = new Client();
-    client.connect(host, function () {
-      client.setVolume({ level: volume }, function (err, response) {
-        console.log("volume restored to ", volume, " for device", host);
-      });
-    });
-  }
-
-  var restorVolumeOfCastedDevies = function () {
-    timeoutRestoreDevicesVolume = null;
-    for (var host in global.castedDevices) {
-      new restorVolumeOfDevice(host, global.castedDevices[host].actualVolume);
-    }
-  }
 
   var onDeviceUp = function (host, url, callback) {
     if (global.castedDevices === undefined) {
@@ -281,16 +164,103 @@ function GoogleHomeNotifier(deviceip, language, speed) {
   };
 
 
+  var restorVolumeOfCastedDevies = function () {
+    timeoutRestoreDevicesVolume = null;
+    for (var host in global.castedDevices) {
+      new restorVolumeOfDevice(host, global.castedDevices[host].actualVolume);
+    }
+  }
+
+  var restorVolumeOfDevice = function (host, volume) {
+    var client = new Client();
+    client.connect(host, function () {
+      client.setVolume({ level: volume }, function (err, response) {
+        console.log("volume restored to ", volume, " for device", host);
+      });
+    });
+  }
+
+  this.setSpeechSpeed = (readSpeed) => {
+    textSpeed = parseFloat(readSpeed);
+    return this;
+  }
+
+  this.setEmitVolume = function (pctVolume) {
+
+    if (pctVolume != undefined) { // if not defined then the (default/last set) emitValue will be used
+      emitVolume = pctVolume;
+      // make sure volume is a percentage, else adjust
+      if (emitVolume > 100) emitVolume = 100;
+      if (emitVolume < 0) emitVolume = 0;
+      if (emitVolume > 1) emitVolume = emitVolume / 100;
+    }
+    return this;
+  }
+
+  this.setCacheFolder = function (cacheFolderToUse) {
+    if (cacheFolderToUse !== "" && cacheFolder !== cacheFolderToUse) {
+      cacheFolder = cacheFolderToUse;
+      // localFileServerRestart()
+    }
+    return this;
+  }
+
+  this.setFileServerPort = function (serverPortToSet) {
+    // if (serverPortToSet !== "" && mediaServerPort !== serverPortToSet) {
+    //   mediaServerPort = serverPortToSet;
+    //   if (cacheFolder !== "") {
+    //     // localFileServerRestart();
+    //   }
+    // }
+    // return this;
+  }
+
+
+  this.play = function (mp3_url, callback) {
+    getPlayUrl(mp3_url, this.deviceip, function (res) {
+      emitter.emit("play", res)
+    });
+  };
+
+
+  var getPlayUrl = function (url, host, callback) {
+    onDeviceUp(host, url, function (res) {
+      callback(res);
+    });
+  };
+
+
+
+
+
 };
 
 GoogleHomeNotifier.prototype.__proto__ = EventEmitter.prototype // inherit from EventEmitter
 
-module.exports = function (deviceip, language, speed) {
+module.exports = function (deviceip, language, speed, mediaServerIp, mediaServerPort,cacheFolder) {
   if (deviceip && language) {
     if (!speed) {
       speed = 1
     };
-    return new GoogleHomeNotifier(deviceip, language, speed);
+    return new GoogleHomeNotifier(deviceip, language, speed,mediaServerIp, mediaServerPort,cacheFolder);
   }
 }
 
+
+
+function Download_Mp3(text, language, fileNameWithSpeedAndLanguage, playSlow, callback) {
+
+  var dstFilePath = path.join(cacheFolder, fileNameWithSpeedAndLanguage);
+  // get base64 text
+  Googletts
+    .getAudioBase64(text, { lang: language, slow: playSlow })
+    .then((base64) => {
+      // console.log({ base64 });
+
+      // save the audio file
+      const buffer = Buffer.from(base64, 'base64');
+      fs.writeFileSync(dstFilePath, buffer, { encoding: 'base64' });
+      callback();
+    })
+    .catch(console.error);
+}
